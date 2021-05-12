b20baff17fc9c95d20ab7c4293719235038ad31c
haribabuv <haribabu.v@hdworks.in>
2021-05-12 12:32:37 +0530

    Added the framework

---
+++ A23RN.framework/Headers/A23RN.h
@@ -0,0 +1,18 @@
+//
+//  A23RN.h
+//  A23RN
+//
+//  Created by Hari on 10/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for A23RN.
+FOUNDATION_EXPORT double A23RNVersionNumber;
+
+//! Project version string for A23RN.
+FOUNDATION_EXPORT const unsigned char A23RNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <A23RN/PublicHeader.h>
+
+#import <A23RN/RandomNumberUtils.h>
